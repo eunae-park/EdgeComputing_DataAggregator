a6593e399328592226bbb3fe57dcc18ade8e0373
cyber-steer <cybersteer@naver.com>
2023-02-22 10:38:55 +0900

    FileManagementDto.java FileManagementDto update

--- src/kr/re/keti/database/FileManagementDto.java
+++ src/kr/re/keti/database/FileManagementDto.java
@@ -42,5 +42,6 @@ public class FileManagementDto{
 	public FileManagementDto(File file, String signature) {
 		int dotIndex = file.getName().lastIndexOf(".");
 		this.dataId = (dotIndex > 0) ? file.getName().substring(0, dotIndex) : file.getName();
+		this.availabilityPolicy = 1;
 	}
 }
