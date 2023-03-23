cd97b8fa9ed8253117451ce46031abf905e959c9
cyber-steer <cybersteer@naver.com>
2023-03-23 12:42:19 +0900

    MysqlDao.java exists if else insert if else select if else select if else update if else delete if else executeQuery if else existsFileUuid if update

--- src/kr/re/keti/database/MysqlDao.java
+++ src/kr/re/keti/database/MysqlDao.java
@@ -184,6 +184,7 @@ public class MysqlDao implements Database {
 		String query = "select dataid from file_uuid whare fileName='" + pk + "'";
 		try (Connection connection = getConnection(); Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query);) {
 			if(resultSet.next()) {
+				return true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
