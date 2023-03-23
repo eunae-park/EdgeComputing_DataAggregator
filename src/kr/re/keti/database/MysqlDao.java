38c1a403e7b409d7f348debdd47546e5b3df3353
cyber-steer <cybersteer@naver.com>
2023-03-23 15:14:24 +0900

    MysqlDao.java exists if else insert if else select if else select if else update if else delete if else executeQuery if else insertFileManagement update

--- src/kr/re/keti/database/MysqlDao.java
+++ src/kr/re/keti/database/MysqlDao.java
@@ -196,6 +196,7 @@ public class MysqlDao implements Database {
 		String insert_sql = "insert into " + "file_management" + " (dataid, availability_policy, cert, data_priority, data_signature, data_size, data_type, directory, file_type, linked_edge, security_level, timestamp)" + " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		try (Connection connection = getConnection(); PreparedStatement pstmt = connection.prepareStatement(insert_sql);) {
 			pstmt.setString(1, dto.getDataId());
+			pstmt.setInt(2, dto.getAvailabilityPolicy());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
