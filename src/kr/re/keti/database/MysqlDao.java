dd4b01614839fb0d04bf32e0cc3465a194433b8d
cyber-steer <cybersteer@naver.com>
2023-03-29 13:34:03 +0900

    MysqlDao.java exists if else insert if else select if else select if else update if else delete if else executeQuery if else selectFileManagement while update

--- src/kr/re/keti/database/MysqlDao.java
+++ src/kr/re/keti/database/MysqlDao.java
@@ -248,6 +248,7 @@ public class MysqlDao implements Database {
 				dto.setDataId(resultSet.getString("dataid"));
 				dto.setAvailabilityPolicy(resultSet.getInt("availability_policy"));
 				dto.setCert(resultSet.getString("cert"));
+				dto.setDataPriority(resultSet.getInt("data_priority"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
