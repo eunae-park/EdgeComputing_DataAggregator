d356b625d4eefd60ddbad983a71bba108df2a46e
cyber-steer <cybersteer@naver.com>
2023-04-06 11:28:32 +0900

    MysqlDao.java exists if else insert if else select if else select if else update if else delete if else executeQuery if else updateFileManagement if

--- src/kr/re/keti/database/MysqlDao.java
+++ src/kr/re/keti/database/MysqlDao.java
@@ -342,6 +342,8 @@ public class MysqlDao implements Database {
 			statement.setString(12, dto.getDataId());
 
 			int check = statement.executeUpdate();
+			if(check == 0) {
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
