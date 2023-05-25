00dff40209092048f20c02eec933b94861dd8fe5
cyber-steer <cybersteer@naver.com>
2023-05-25 15:27:41 +0900

    Broadcast receive

--- src/kr/re/keti/os/Broadcast.java
+++ src/kr/re/keti/os/Broadcast.java
@@ -72,4 +72,7 @@ public class Broadcast {
 			e.printStackTrace();
 		}
 	}
+	//------------------------receive packet-----------------------------------------
+	public DatagramPacket receive() {
+	}
 }
