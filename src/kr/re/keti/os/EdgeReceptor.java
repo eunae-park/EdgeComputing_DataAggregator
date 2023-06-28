09331e87cb68b2b3441cd1c4e72594d33e13b4db
cyber-steer <cybersteer@naver.com>
2023-06-28 15:22:06 +0900

    EdgeReceptor logWrite update

--- src/kr/re/keti/os/EdgeReceptor.java
+++ src/kr/re/keti/os/EdgeReceptor.java
@@ -163,5 +163,6 @@ public class EdgeReceptor {
 
 	public boolean logWrite(String slaveAddr) {
 		SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date nowTime = new Date();
 	}
 }
