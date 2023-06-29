1a258e81887bd722628596711573fe804f641b20
cyber-steer <cybersteer@naver.com>
2023-06-29 13:12:44 +0900

    EdgeReceptor logWrite update

--- src/kr/re/keti/os/EdgeReceptor.java
+++ src/kr/re/keti/os/EdgeReceptor.java
@@ -175,6 +175,7 @@ public class EdgeReceptor {
 		try {
 			FileWriter writer = new FileWriter("edge_ipList.txt", false);
 			writer.write("master\n");
+			writer.flush();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
