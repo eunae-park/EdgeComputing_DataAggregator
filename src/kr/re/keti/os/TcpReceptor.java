68ceb93a592c5d8cef804b53c78b3fc218b2b187
cyber-steer <cybersteer@naver.com>
2023-06-30 13:36:58 +0900

    TcpReceptor update

--- src/kr/re/keti/os/TcpReceptor.java
+++ src/kr/re/keti/os/TcpReceptor.java
@@ -17,4 +17,5 @@ import kr.re.keti.PortNum;
 public class TcpReceptor {
 	public static ArrayList<String> slaveList = new ArrayList<>();
 	private HashMap<String, String> slaveMap = new HashMap<>();
+	private final int CAPACITY = 5000;
 }
