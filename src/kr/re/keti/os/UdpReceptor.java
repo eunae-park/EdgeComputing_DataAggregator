29e91b3b00125f677f31b9b38faf035962e3919a
cyber-steer <cybersteer@naver.com>
2023-07-10 12:38:37 +0900

    UdpReceptor update

--- src/kr/re/keti/os/UdpReceptor.java
+++ src/kr/re/keti/os/UdpReceptor.java
@@ -27,4 +27,5 @@ public class UdpReceptor {
 	public final int DEFAULT_RECEIVE_PORT = PortNum.DEFAULT_RECEIVE_PORT;
 	public final int DEFAULT_SEND_PORT = PortNum.DEFAULT_SEND_PORT;
 	private final int DEFAULT_BUF_LENGTH = 64;
+	private final String DEFAULT_BROADCAST_ADDRESS = "255.255.255.255";
 }
