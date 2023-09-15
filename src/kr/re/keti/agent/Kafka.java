0d05813f251a7e93394f4f7ba7972e05202e8bbd
cyber-steer <cybersteer@naver.com>
2023-09-15 11:40:05 +0900

    Kafka structor update

--- src/kr/re/keti/agent/Kafka.java
+++ src/kr/re/keti/agent/Kafka.java
@@ -38,5 +38,6 @@ public class Kafka {
 
 	public Kafka(String topic, ArrayBlockingQueue<AgentPacket> sendQueue, ArrayBlockingQueue<AgentPacket> receiveQueue) {
 		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "OFF");
+
 	}
 }
