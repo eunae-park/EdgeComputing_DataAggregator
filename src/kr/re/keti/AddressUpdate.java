70f85f55979e8bbf326ceb2cc9dbd0201b80720d
cyber-steer <cybersteer@naver.com>
2023-10-24 11:02:12 +0900

    AddressUpdate import add

---
+++ src/kr/re/keti/AddressUpdate.java
@@ -0,0 +1,11 @@
+package kr.re.keti;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+
