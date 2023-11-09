38baa710010ac85a9349f771603a2f8cb00b3355
cyber-steer <cybersteer@naver.com>
2023-11-09 12:16:54 +0900

    Command for if

--- src/kr/re/keti/Command.java
+++ src/kr/re/keti/Command.java
@@ -117,5 +117,7 @@ public class Command extends Thread{
 	}
 	private void wholeDataInformation(String command) {
 		String[] commands = command.split(" ");
+		if(commands.length == 1 || commands[1].equals(Main.deviceIP)) {
+		}
 	}
 }
