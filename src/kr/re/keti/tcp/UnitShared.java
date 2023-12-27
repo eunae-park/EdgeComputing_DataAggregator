84e4808c7a7644e5d86a00e586f5dfc5dd8e5721
cyber-steer <cybersteer@naver.com>
2023-12-27 16:07:38 +0900

    UnitShared chunkCreate while none update

--- src/kr/re/keti/tcp/UnitShared.java
+++ src/kr/re/keti/tcp/UnitShared.java
@@ -272,6 +272,7 @@ public class UnitShared {
 	                threadLogBuilder.append(Main.deviceIP).append("#").append(this.uuid).append("#").append(this.fileName).append("#").append(this.securityLevel).append("#loding(").append(chunkStatus).append("%)#").append(chunkStatus);
 	                String threadLog = threadLogBuilder.toString();
 
+	                byte[] threadMessage = DataProcess.messageCreate("REQ", Main.deviceIP, "300", threadLog);
 	}
 	}
 	} catch (Exception e) {
