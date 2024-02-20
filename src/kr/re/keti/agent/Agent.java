9d1473d00304850fb651c363775471915f2a4555
cyber-steer <cybersteer@naver.com>
2024-02-20 16:16:21 +0900

    Agent tcpProcess switch update

--- src/kr/re/keti/agent/Agent.java
+++ src/kr/re/keti/agent/Agent.java
@@ -177,6 +177,7 @@ public class Agent extends EdgeDataAggregator{
 			case 405:
 				String dataid = datas[3];
 				int startIdx = Integer.parseInt(datas[4]);
+				int finishIdx = Integer.parseInt(datas[5]);
 		}
 	}
 }
